83a5f9c564c7229e22189b76e7f9caa75f732f28
Ксения <94642483+ksssharova@users.noreply.github.com>
2022-04-24 16:55:18 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,68 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include string
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct ITEM {
+    T value;
+    ITEM* next, * prev;
+  };
+  
+  ITEM* head, * tail;
+  
+  TPQueue::ITEM* create(cinst T& value) {
+    ITEM* item = new ITEM;
+    item->value = value;
+    item->next = item->prev = nullptr;
+    return item;
+  }
+  
+ public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
+  ~TPQueue() {
+    while (head)
+      pop();
+  }
+  
+  void push(const T& value) {
+        ITEM* temp = head;
+        ITEM* item = create(value);
+        while (temp && temp->value.prior >= value.prior)
+            temp = temp->next;
+        if (!temp && head) {
+            tail->next = item;
+            item->prev = tail;
+            tail = item;
+        } else if (!temp && !head) {
+            head = tail = item;
+        } else if (!temp->prev) {
+            head->prev = item;
+            item->next = head;
+            head = item;
+        } else {
+            temp->prev->next = item;
+            item->prev = temp->prev;
+            item->next = temp;
+            temp->prev = item;
+        }
+    }
+  
+  T pop() {
+    if (!head || !tail)
+      throw std::string("The queue is empty!");
+    ITEM* temp = head->next;
+    if (temp)
+      temp->prev = nullptr;
+    T value = head->value;
+    delete head;
+    head = temp;
+    if (!head)
+      tail = nullptr;
+    return value;
+  }
 };
 
 struct SYM {
